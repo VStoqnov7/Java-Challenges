c45ff621ceef180e8a9c287c7839b1bcc28d7f7a
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2024-12-28 01:31:07 +0200

    Update

--- LargestHourglassSum/LargestHourglassSum.java
+++ LargestHourglassSum/LargestHourglassSum.java
@@ -26,5 +26,20 @@ public class LargestHourglassSum {
         }
 
         bufferedReader.close();
+
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int currentSum =
+                        arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) // Горен ред
+                                + arr.get(i + 1).get(j + 1) // Среден елемент
+                                + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2); // Долен ред
+
+                maxSum = Math.max(maxSum, currentSum);
+            }
+        }
+
+        System.out.println(maxSum);
     }
 }
